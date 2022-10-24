e655eec01249583e439cb111ddeda80a15b18adc
Dealina Susilowati <dealinaaa@gmail.com>
2022-10-24 22:01:36 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Dealina Susilowati";
+    $umur = 19;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaAdik = "Dhimas Bayu Prasetyo";
+    $umurAdik = "14";
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, saya berusia $umurAdik tahun.<br>";
+
+    $selisihUmur = $umur - $umurAdik;
+    echo "selisih usia saya dan adik saya adalah $selisihUmur tahun."
+
+?>
